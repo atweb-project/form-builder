f3822c2789d8a5e8406142e72475763881afae7c
Dimitrios Kostaras <kostardi@aris-lux.lan>
2019-02-28 01:50:42 +0200

    add mongoose and database providers

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { Database } from './database';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, Database],
 })
 export class AppModule {}
