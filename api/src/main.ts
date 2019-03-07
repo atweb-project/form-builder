db5709886c8b8185da9b1618d20184f6282e6f3a
Dimitrios Kostaras <kostardi@aris-lux.lan>
2019-03-07 22:19:50 +0200

    change the url of swagger docs

--- api/src/main.ts
+++ api/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/docs/swagger', app, document);
   await app.listen(3000);
 }
 bootstrap();
